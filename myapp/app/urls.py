8fa368c963ad75702024f0deb5982c63dac2d75c
beerbank3 <yg050140528@gmail.com>
2023-06-26 17:41:36 +0900

    폴더 이름 변경

--- myapp/app/urls.py
+++ myapp/app/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    # index_page
+    path('', views.IndexMain.as_view()),
     path('admin/', admin.site.urls),
 ]
