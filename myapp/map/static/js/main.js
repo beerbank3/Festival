55375dc8ce5b522f91affb35c3aec2da53f85cc9
beerbank3 <yg050140528@gmail.com>
2023-07-03 17:35:03 +0900

    js정리, 메인페이지 시간 기능 추가

--- myapp/map/static/js/main.js
+++ myapp/map/static/js/main.js
@@ -1,62 +1,14 @@
-const loader = document.querySelector(".loader");
-
-let pageNo = 2;
-let isScrollEventEnabled = true;
-
-function loadMoreContent(callback) {
-    // AJAX 요청 보내기
-    const url = "tour_data/loaderFestivalList?pageNo="+encodeURIComponent(pageNo)
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-    // 응답 처리
-    if (data.content) {
-        // 응답 데이터를 HTML 형식으로 변환하여 loader에 추가
-        let contentHTML = "";
-        for (let item of data.content.item) {
-            contentHTML += '<div class="col mb-5">';
-            contentHTML += '<div class="card h-100">';
-            contentHTML += '<img class="img-thumbnail card-img-height" src="' + item.firstimage + '" alt="..." />';
-            contentHTML += '<div class="card-body p-4">';
-            contentHTML += '<div class="text-center">';
-            contentHTML += '<h5 class="fw-bolder">' + item.title + '</h5>';
-            contentHTML += '<div class="date">' + item.eventstartdate.slice(0, 4) + '.' + item.eventstartdate.slice(4, 6) + '.' + item.eventstartdate.slice(6, 8) + ' ~ ' + item.eventenddate.slice(0, 4) + '.' + item.eventenddate.slice(4, 6) + '.' + item.eventenddate.slice(6, 8) + '</div>';
-            contentHTML += '</div>';
-            contentHTML += '</div>';
-            contentHTML += '</div>';
-            contentHTML += '</div>';
-        }
-        loader.innerHTML += contentHTML;
-        pageNo++;
-        if (typeof callback === 'function') {
-            callback();
-        }
-    } else {
-        loader.innerHTML += "더 이상 콘텐츠가 없습니다.";
-    }
-    })
-    .catch(error => {
-    console.error(error);
-    });
+const time = document.querySelector(".time");
+function getCurrentTime() {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = ("0" + (now.getMonth() + 1)).slice(-2);
+    let day = ("0" + now.getDate()).slice(-2);
+    let hours = ("0" + now.getHours()).slice(-2);
+    let minutes = ("0" + now.getMinutes()).slice(-2);
+    
+    let formattedTime = year + "년 " + month + "월 " + day + "일 " + hours + "시 " + minutes + "분";
+    time.innerHTML = formattedTime;
 }
 
-function handleScroll() {
-    if (!isScrollEventEnabled) {
-        return;
-    }
-
-    const scrollHeight = document.documentElement.scrollHeight;
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const clientHeight = window.innerHeight || document.documentElement.clientHeight;
-
-    if (scrollHeight - scrollTop <= clientHeight + 200) {
-        isScrollEventEnabled = false; // 스크롤 이벤트 비활성화
-
-        loadMoreContent(function() {
-            isScrollEventEnabled = true; // loadMoreContent 함수 실행이 완료된 후 스크롤 이벤트 다시 활성화
-        });
-    }
-}
-
-window.addEventListener("scroll", handleScroll);
-
+setInterval(getCurrentTime, 1000);  // 1초마다 시간 업데이트
